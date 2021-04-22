864027ed45967a2e1c29f13046ad144c9b6bf3b4
Luisa Mårtensson <luisa_martensson@hotmail.com>
2021-04-22 15:48:36 +0200

    starup owin

---
+++ FordonsregistretProjekt/FordonsregistretProjekt/App_Start/Startup1.cs
@@ -0,0 +1,38 @@
+﻿using FordonsregistretProjekt.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FordonsregistretProjekt.App_Start
+{
+    public class Startup1
+    {
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        //    app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+        //    var newtonProvider = new OAuthWebApiAuthorizationServerProvider();
+        //    OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+        //    {
+        //        AllowInsecureHttp = true,
+        //        TokenEndpointPath = new PathString("/token"),
+        //        AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+        //        Provider = newtonProvider
+        //    };
+
+        //    app.UseOAuthAuthorizationServer(options);
+        //    app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        //    HttpConfiguration config = new HttpConfiguration();
+        //    WebApiConfig.Register(config);
+
+
+        //}
+    }
+}
